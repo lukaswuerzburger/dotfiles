bfdfd06f90baaa89b0696b648a31acd4db9807f5
Lukas Würzburger <lwuerzburger@babbel.com>
2021-01-06 16:08:30 +0100

    Clean up

--- setup/general.sh
+++ setup/general.sh
@@ -1,5 +1,5 @@
+# Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install mas
-
+# Show hidden files
 defaults write com.apple.finder AppleShowAllFiles YES
